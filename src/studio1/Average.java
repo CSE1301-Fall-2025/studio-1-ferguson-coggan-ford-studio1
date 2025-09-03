ddff9b3039a1c6a583e75f3cea6b6d2d2c196892
Gavin Coggan <coggan@wustl.edu>
2025-09-03 15:27:48 -0500

    real finished

--- src/studio1/Average.java
+++ src/studio1/Average.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("enter value for n1");
-        int n1 = in.nextInt();
-        System.out.print("enter value for n2");
-        int n2 = in.nextInt();
-        int n3 = (n1 + n2) / 2;
-        System.out.print(n3);
+        System.out.print("Enter value for N1: ");
+        double n1 = in.nextDouble();
+        System.out.print("Enter value for N2: ");
+        double n2 = in.nextDouble();
+        double n3 = (n1 + n2) / 2;
+        System.out.print("Average of "+n1+" and "+n2+"is"+n3);
 }
 }
